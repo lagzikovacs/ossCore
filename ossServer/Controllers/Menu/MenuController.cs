aa9a2292c1894a3373732f4ccfac9dc4527c65d7
lagzikovacs <lks@docport.hu>
2019-05-04 13:45:25 +0200

    MenuController ++

--- ossServer/Controllers/Menu/MenuController.cs
+++ ossServer/Controllers/Menu/MenuController.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using ossServer.Models;
 using ossServer.Utils;
+using System;
+using System.Threading.Tasks;
 
 namespace ossServer.Controllers.Menu
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class MenuController : ControllerBase
     {
